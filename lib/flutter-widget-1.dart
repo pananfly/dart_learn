d4d249a0209ec589c47a9875ecf5b2b7be777077
pananfly <906792655@qq.com>
2020-11-13 16:26:07 +0800

    Update codes.

--- lib/flutter-widget-1.dart
+++ lib/flutter-widget-1.dart
@@ -1,19 +1,50 @@
+import 'dart:convert';
+import 'dart:isolate';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String EMAIL_KEY = "flutter_email";
 
 void main() {
   runApp(MyApp());
 }
 
+showToast(String msg) {
+  Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.blue,
+      textColor: Colors.white,
+      fontSize: 16.0
+  );
+}
+
+Future<void> saveEmail() async {
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await preferences.setString(EMAIL_KEY, "admin@pananfly.com");
+}
+
+Future<String> getEmail() async {
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  return preferences.getString(EMAIL_KEY);
+}
+
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    saveEmail();
     return MaterialApp(
       title: 'Flutter Widget 1',
       theme: ThemeData(
         primarySwatch: Colors.red,
+        textSelectionTheme: TextSelectionThemeData(selectionColor: Colors.blue),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Sample1(title: 'Flutter Widget 1 Test'),
@@ -21,6 +52,11 @@ class MyApp extends StatelessWidget {
         '1': (BuildContext ctx) => Sample2(title: "Fade Test"),
         '2': (BuildContext ctx) => Sample3(title: "Canvas Test"),
         '3': (BuildContext ctx) => Sample4(title: "Custom widget Test"),
+        '4': (BuildContext ctx) => Sample5(title: "Http Test"),
+        '5': (BuildContext ctx) => Sample6(title: "Http Isolate Test"),
+        '6': (BuildContext ctx) => Sample7(title: "LifeCycle Test"),
+        '7': (BuildContext ctx) => Sample8(title: "Gesture Test"),
+        '8': (BuildContext ctx) => Sample9(title: "Edit Test"),
       },
     );
   }
@@ -33,33 +69,6 @@ class Sample1 extends StatefulWidget {
 
   @override
   State createState() => _Sample1State();
-}
-
-class Sample2 extends StatefulWidget {
-  final String title;
-
-  Sample2({Key key, this.title}) : super(key: key);
-
-  @override
-  State createState() => _Sample2State();
-}
-
-class Sample3 extends StatefulWidget {
-  final String title;
-
-  Sample3({Key key, this.title}) : super(key: key);
-
-  @override
-  State createState() => _Sample3State();
-}
-
-class Sample4 extends StatefulWidget {
-  final String title;
-
-  Sample4({Key key, this.title}) : super(key: key);
-
-  @override
-  State createState() => _Sample4State();
 }
 
 String sample1Txt = "pananfly";
@@ -95,39 +104,87 @@ class _Sample1State extends State<Sample1> {
           mainAxisSize: MainAxisSize.max,
           children: [
             _getChild(),
-            MaterialButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: Text("Fade test"),
-              onPressed: () => Navigator.of(context).pushNamed("1"),
-            ),
-            MaterialButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: Text("Canvas test"),
-              onPressed: () => Navigator.of(context).pushNamed("2"),
-            ),
-            MaterialButton(
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: Text("Custom test"),
-              onPressed: () {
-                Navigator.of(context)
-                    .pushNamed("3")
-                    .then(
-                        (msg) =>
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Fade test"),
+                  onPressed: () => Navigator.of(context).pushNamed("1"),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Canvas test"),
+                  onPressed: () => Navigator.of(context).pushNamed("2"),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Custom test"),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushNamed("3")
+                        .then(
+                            (msg) =>
                             Fluttertoast.showToast(
-                            msg: msg == null ? "Unknown" : msg.toString(),
-                            toastLength: Toast.LENGTH_LONG,
-                            gravity: ToastGravity.BOTTOM,
-                            backgroundColor: Colors.blue,
-                            textColor: Colors.white,
-                            fontSize: 16.0
-                        ));
-              },
-            )
-          ],
-        ),
+                                msg: msg == null ? "Unknown" : msg.toString(),
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.BOTTOM,
+                                backgroundColor: Colors.blue,
+                                textColor: Colors.white,
+                                fontSize: 16.0
+                            ));
+                  },
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage("assets/images/test.png"),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Http test"),
+                  onPressed: () => Navigator.of(context).pushNamed("4"),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Http isolate test"),
+                  onPressed: () => Navigator.of(context).pushNamed("5"),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("LifeCycle test"),
+                  onPressed: () => Navigator.of(context).pushNamed("6"),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Gesture test"),
+                  onPressed: () => Navigator.of(context).pushNamed("7"),
+                ),
+                MaterialButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text("Edit test"),
+                  onPressed: () => Navigator.of(context).pushNamed("8"),
+                ),
+              ],
+            ),
+          ]
+        )
       ),
       floatingActionButton: FloatingActionButton (
         onPressed: _updateText,
@@ -138,6 +195,16 @@ class _Sample1State extends State<Sample1> {
   }
 }
 
+
+class Sample2 extends StatefulWidget {
+  final String title;
+
+  Sample2({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample2State();
+}
+
 class _Sample2State extends State<Sample2> with TickerProviderStateMixin {
 
   AnimationController _animationController;
@@ -145,7 +212,6 @@ class _Sample2State extends State<Sample2> with TickerProviderStateMixin {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _animationController = AnimationController(
       duration: const Duration(milliseconds: 2000),
@@ -156,7 +222,6 @@ class _Sample2State extends State<Sample2> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -181,6 +246,14 @@ class _Sample2State extends State<Sample2> with TickerProviderStateMixin {
   }
 }
 
+class Sample3 extends StatefulWidget {
+  final String title;
+
+  Sample3({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample3State();
+}
 
 class _Sample3State extends State<Sample3>{
 
@@ -188,7 +261,6 @@ class _Sample3State extends State<Sample3>{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
 
       appBar: AppBar(
@@ -236,6 +308,15 @@ class SignaturePainter extends CustomPainter {
   bool shouldRepaint(covariant SignaturePainter oldDelegate) => oldDelegate.points != points;
 }
 
+class Sample4 extends StatefulWidget {
+  final String title;
+
+  Sample4({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample4State();
+}
+
 class _Sample4State extends State<Sample4>{
   @override
   Widget build(BuildContext context) {
@@ -260,5 +341,298 @@ class _Sample4Button extends StatelessWidget {
     return ElevatedButton(onPressed: () {
       Navigator.of(context).pop("Result from custom widget.");
     }, child: Text(content), style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.amber)),);
+  }
+}
+
+
+class Sample5 extends StatefulWidget {
+  final String title;
+
+  Sample5({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample5State();
+}
+
+class _Sample5State extends State<Sample5> {
+
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title)
+      ),
+      body: ListView.builder(
+        itemCount: widgets.length,
+        itemBuilder: (BuildContext ctx, int position) {
+          return getRow(position);
+        },
+      ),
+    );
+  }
+
+  Widget getRow(int i) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Text("${i + 1}: Row ${widgets[i]["title"]}"),
+    );
+  }
+
+  Future<void> loadData() async {
+    String url = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(url);
+    setState(() {
+      widgets = jsonDecode(response.body);
+    });
+  }
+}
+
+class Sample6 extends StatefulWidget {
+  final String title;
+
+  Sample6({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample6State();
+}
+
+class _Sample6State extends State<Sample6> {
+
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text(widget.title)
+        ),
+        body: getBody(),
+    );
+  }
+
+  bool showLoadingDialog() {
+      return widgets == null || widgets.length <= 0;
+  }
+
+  Widget getBody() {
+    if(showLoadingDialog()) {
+      return getProgressDialog();
+    }
+    return ListView.builder(
+        itemCount: widgets.length,
+        itemBuilder: (BuildContext ctx, int position) {
+          return getRow(position);
+        });
+  }
+
+  Widget getRow(int i) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: GestureDetector(
+        child: Text("${i + 1}: Row ${widgets[i]["title"]}"),
+        onTap: () => showToast("Click position: $i, data: ${widgets[i]}"),
+      ),
+    );
+  }
+
+  Widget getProgressDialog() {
+    return Center(child: CircularProgressIndicator(),);
+  }
+
+  Future<void> loadData() async {
+    ReceivePort receivePort = ReceivePort();
+    await Isolate.spawn(dataLoader, receivePort.sendPort);
+
+    SendPort sendPort = await receivePort.first;
+    List msg = await sendReceive(sendPort, "https://jsonplaceholder.typicode.com/posts");
+    setState(() {
+      widgets = msg;
+    });
+  }
+
+  static Future<void> dataLoader(SendPort sendPort) async {
+    ReceivePort port = ReceivePort();
+    sendPort.send(port.sendPort);
+
+    await for (var msg in port) {
+      String data = msg[0];
+      SendPort replyTo = msg[1];
+
+      String url = data;
+      http.Response response = await http.get(url);
+      replyTo.send(jsonDecode(response.body));
+    }
+  }
+
+  Future sendReceive(SendPort port, msg) {
+    ReceivePort response = ReceivePort();
+    port.send([msg, response.sendPort]);
+    return response.first;
+  }
+}
+
+class Sample7 extends StatefulWidget {
+  final String title;
+
+  Sample7({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample7State();
+}
+
+class _Sample7State extends State<Sample7> with WidgetsBindingObserver {
+
+  AppLifecycleState _state;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    setState(() {
+      _state = state;
+      Fluttertoast.showToast(
+          msg: "State: $state",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.blue,
+      textColor: Colors.white,
+          fontSize: 16.0
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title)
+      ),
+      body: Center(
+        child: Text("Cur lifecycle state: $_state"),
+      ),
+    );
+  }
+}
+
+class Sample8 extends StatefulWidget {
+  final String title;
+
+  Sample8({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample8State();
+}
+
+class _Sample8State extends State<Sample8>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title)
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: FlutterLogo(size: 500,),
+          onTap: () => showToast("Action: onTap()"),
+          onTapDown: (TapDownDetails details) => showToast("Action: onTapDown()"),
+          onTapUp: (TapUpDetails details) => showToast("Action: onTapUp()"),
+          onTapCancel: () => showToast("Action: onTapCancel()"),
+          onDoubleTap: () => showToast("Action: onDoubleTap()"),
+          onLongPress: () => showToast("Action: onLongPress()"),
+          onVerticalDragStart: (DragStartDetails details) => showToast("Action: onVerticalDragStart()"),
+          onVerticalDragUpdate: (DragUpdateDetails details) => showToast("Action: onVerticalDragUpdate()"),
+          onVerticalDragEnd: (DragEndDetails details) => showToast("Action: onVerticalDragEnd()"),
+          onHorizontalDragStart: (DragStartDetails details) => showToast("Action: onHorizontalDragStart()"),
+          onHorizontalDragUpdate: (DragUpdateDetails details) => showToast("Action: onHorizontalDragUpdate()"),
+          onHorizontalDragEnd: (DragEndDetails details) => showToast("Action: onHorizontalDragEnd()"),
+        )
+      ),
+    );
+  }
+}
+
+class Sample9 extends StatefulWidget {
+  final String title;
+
+  Sample9({Key key, this.title}) : super(key: key);
+
+  @override
+  State createState() => _Sample9State();
+}
+
+class _Sample9State extends State<Sample9>{
+
+  String _errorTxt;
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // get text from SharePreference
+    getEmail().then((value) => _textEditingController.text = value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(widget.title)
+      ),
+      body: Center(
+        child: TextField(
+          controller: _textEditingController,
+          onSubmitted: (String txt) {
+            setState(() {
+              if(isEmail(txt)) {
+                _errorTxt = null;
+              } else {
+                _errorTxt = "Error, this is not an email.";
+              }
+            });
+          },
+          onChanged: (String txt) => setState(() {_errorTxt = null;}),
+          decoration: InputDecoration(
+            hintText: "This is a hint",
+            errorText: _errorTxt,
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool isEmail(String em) {
+    String emailRegexp =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+    RegExp regExp = RegExp(emailRegexp);
+
+    return regExp.hasMatch(em);
   }
 }
