082a71e56498a16e9499f043207b1a61dd7d39a9
pananfly <906792655@qq.com>
2020-11-26 17:48:54 +0800

    Update codes

--- lib/layout_build/Layout-Sample1.dart
+++ lib/layout_build/Layout-Sample1.dart
@@ -11,40 +11,84 @@ class LayoutSample1 extends StatelessWidget {
   Widget build(BuildContext context) {
 
     Widget _buildImage() {
-      return Container(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Image.asset("assets/images/cake.jpg", fit: BoxFit.fill,),
-      );
+      return Image.asset("assets/images/cake.jpg", fit: BoxFit.cover,);
     }
 
     Widget _buildStar() {
-      return Row(
-        children: [
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  padding: EdgeInsets.only(bottom: 8),
-                  child: Text("Oeschinen Lake Campground",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold
+      return Container(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                verticalDirection: VerticalDirection.down,
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Text("Oeschinen Lake Campground",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold
+                      ),
                     ),
                   ),
-                ),
-                Text("Kandersteg, Switzerland",
-                  style: TextStyle(
-                      color: Colors.grey
+                  Text("Kandersteg, Switzerland",
+                    style: TextStyle(
+                        color: Colors.grey
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red,),
-          Text('41')
-        ],
+            Icon(Icons.star, color: Colors.red,),
+            Text('41')
+          ],
+        ),
       );
     }
+
+    Widget _buildActionColumn(Color color, IconData icon, String label) =>
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: color,),
+            Container(
+              padding: EdgeInsets.only(top: 8),
+              child: Text(
+                label,
+                style: TextStyle(
+                  color: color
+                ),
+              ),
+            )
+          ],
+        );
+
+    Widget _buildAction() => Container(
+      padding: EdgeInsets.fromLTRB(32, 2, 32, 2),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildActionColumn(Colors.blue, Icons.call, "CALL"),
+          _buildActionColumn(Colors.blue, Icons.near_me, "ROUTE"),
+          _buildActionColumn(Colors.blue, Icons.share, "SHARE"),
+        ],
+      ),
+    );
+
+    Widget _descriptSection() => Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -55,6 +99,8 @@ class LayoutSample1 extends StatelessWidget {
           children: [
             _buildImage(),
             _buildStar(),
+            _buildAction(),
+            _descriptSection(),
           ],
         ),
       ),
